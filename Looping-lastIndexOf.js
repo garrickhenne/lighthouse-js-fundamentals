b629cd99f0fb43f7b924f8fcb0cb021c19803786
Garrick Henne <garrickhenne@outlook.com>
2023-07-11 20:51:29 -0600

    Looping-lastIndexOf

---
+++ Looping-lastIndexOf.js
@@ -0,0 +1,15 @@
+function lastIndexOf(arr, value) {
+  let count = -1;
+  arr.forEach((item, index) => {
+    if (item === value) {
+      count = index;
+    }
+  });
+  return count;
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
